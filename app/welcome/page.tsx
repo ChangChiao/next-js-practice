fcbb34eabdc717530d0cd6afce03f1f0b90fc7d9
JoeCT_Chang <JoeCT.Chang@moxa.com>
2024-05-07 09:17:33 +0800

    feat: add meals db

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -1,3 +1,7 @@
-export default function Welcome() {
+import { getMeals } from "@/lib/meal";
+
+export default async function Welcome() {
+  const meals = await getMeals();
+  console.log("meals", meals);
   return <h1>Welcome to Create Next App!</h1>;
 }
