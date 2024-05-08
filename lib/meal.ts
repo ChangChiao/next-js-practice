824c75f8ef659ce90f070bc2d5d01e522960348c
joe <JoeCT_Chang@moxa.com>
2024-05-08 20:19:07 +0800

    feat: add sql query for single meal

--- lib/meal.ts
+++ lib/meal.ts
@@ -5,3 +5,7 @@ const db = sql("meals.db");
 export const getMeals = () => {
   return db.prepare("SELECT * FROM meals").all();
 };
+
+export const getMeal = (id: number) => {
+  return db.prepare("SELECT * FROM meals WHERE id = ?").get(id);
+};
